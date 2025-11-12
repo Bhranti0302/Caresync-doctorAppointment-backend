894d05cdd73b9403d57e61586bff38c74afa13e0
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2025-11-12 16:55:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ import appointmentRoutes from "./routes/appointmentRoutes.js";
 // -----------------------------------------------------------------------------
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
-console.log("✅ Loaded ENV:", {
-  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
-    ? "✅ Exists"
-    : "❌ Missing",
-});
+// console.log("✅ Loaded ENV:", {
+//   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+//   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+//   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+//     ? "✅ Exists"
+//     : "❌ Missing",
+// });
 
 // -----------------------------------------------------------------------------
 // 2️⃣ Express App Initialization
