1ae8d99e28078856206fb16249aa84736d2c6a0c
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2025-11-27 18:54:22 +0530

    fix index.js and gitignore

--- index.js
+++ index.js
@@ -54,14 +54,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/appointments", appointmentRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
-  });
-}
-
 // 3. Error handlers (404 / error middleware)
 app.use(notFound);
 app.use(errorHandler);
